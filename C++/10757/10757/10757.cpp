0fd4f3cefb0d04d782917ef874be43f33e9f0455
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-05-05 22:34:38 +0900

    주석 추가

--- C++/10757/10757/10757.cpp
+++ C++/10757/10757/10757.cpp
@@ -1,76 +1,88 @@
+//백준 10757 : 큰 수 A+B
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
-    vector<char> in_num1, in_num2;
+    vector<char> vec_in_num1, vec_in_num2;
+    string str_in_num1, str_in_num2;
 
-    string temp, temp2;
+    // 두가지 인풋을 ' '을 기준으로 잘라서 str로 받아줌
+    getline(cin, str_in_num1, ' ');
+    getline(cin, str_in_num2);
 
-    getline(cin, temp, ' ');
-    getline(cin, temp2);
+    //받아준 str 인풋의 각 길이를 저장
+    int str1_size = str_in_num1.size();
+    int str2_size = str_in_num2.size();
 
-    int temp_size = temp.size();
-    int temp2_size = temp2.size();
+    //입력값의 벡터 컴포넌트화
+    for (int i = 0; i < str1_size; i++)
+        vec_in_num1.push_back(str_in_num1[i]);
+    for (int i = 0; i < str2_size; i++)
+        vec_in_num2.push_back(str_in_num2[i]);
 
-    ///////////////////입력값의 벡터 컴포넌트화///////////////////////
-    for (int i = 0; i < temp_size; i++)
-        in_num1.push_back(temp[i]);
-
-    for (int i = 0; i < temp2_size; i++)
-        in_num2.push_back(temp2[i]);
-
+    //long_length = 2가지 인풋 중 긴 것을 저장
     int long_length = 0;
-    if (temp_size > temp2_size)
-        long_length = temp_size;
+    if (str1_size > str2_size)
+        long_length = str1_size;
     else
-        long_length = temp2_size;
+        long_length = str2_size;
 
     vector<char>::iterator it;
-    if (long_length > temp_size)
+    //this_length = 2가지 인풋 중 짧은 것을 저장
+    int this_length = 0;
+
+    //2가지 인풋의 전체 길이를 temp 값 0을 삽입하여 long_length에 맞춤
+    if (long_length > str1_size)
     {
-        int this_len = long_length - temp_size;
-        for (int i = 0; i < this_len; i++)
+        this_length = long_length - str1_size;
+        for (int i = 0; i < this_length; i++)
         {
-            it = in_num1.begin();
-            in_num1.insert(it, '0');
+            it = vec_in_num1.begin();
+            vec_in_num1.insert(it, '0');
         }
     }
-    else if (long_length > temp2_size)
+    else if (long_length > str2_size)
     {
-        int this_len = long_length - temp2_size;
-        for (int i = 0; i < this_len; i++)
+        this_length = long_length - str2_size;
+        for (int i = 0; i < this_length; i++)
         {
-            it = in_num2.begin();
-            in_num2.insert(it, '0');
+            it = vec_in_num2.begin();
+            vec_in_num2.insert(it, '0');
         }
     }
 
-    /////////////////////////////////////////////////
-    int* int_arr = new int[long_length];
+    int* p_result_arr = new int[long_length];
     int int_temp1, int_temp2;
 
+    //char 벡터를 int로 형변환하고 두 인풋 값을 더해줌
+    //p_result_arr[i] = vec_in_num[i] + vec_in_num2[i]
     for (int i = 0; i < long_length; i++)
     {
-        int_temp1 = in_num1[i] - '0';
-        int_temp2 = in_num2[i] - '0';
-        int_arr[i] = int_temp1 + int_temp2;
+        int_temp1 = vec_in_num1[i] - '0';
+        cout << int_temp1 << endl;
+        int_temp2 = vec_in_num2[i] - '0';
+        cout << int_temp2 << endl;
+        p_result_arr[i] = int_temp1 + int_temp2;
+        cout << p_result_arr[i] <<endl<<endl;
     }
 
-    for (int i = long_length - 1; i >= 1; i--)
+    //p_result_arr 배열 인덱스 중 10 이상인 인덱스의 경우 반올림
+    //배열 0번째 인덱스의 경우 반올림 연산을 해줄 필요가 없음
+    for (int i = long_length - 1; i > 0; i--)
     {
-        if (int_arr[i] >= 10)
+        if (p_result_arr[i] > 9)
         {
-            int_arr[i] -= 10;
-            int_arr[i - 1] += 1;
+            p_result_arr[i] -= 10;
+            p_result_arr[i - 1] += 1;
         }
     }
 
+    //전체 숫자 길이인 long_length만큼 출력
     for (int i = 0; i < long_length; i++)
-        cout << int_arr[i];
+        cout << p_result_arr[i];
 
     return 0;
 }
