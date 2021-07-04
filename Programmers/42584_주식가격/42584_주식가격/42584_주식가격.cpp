6bb4b663d551daea66c911af62f67c06cad47c9d
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-07-04 22:38:31 +0900

    42584_주식가격

---
+++ Programmers/42584_주식가격/42584_주식가격/42584_주식가격.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices);
+
+int main()
+{
+	vector<int> prices1 = { 1, 2, 3, 2, 3 };
+	vector<int> prices2 = { 5,4,5,2,5 };
+
+	vector<int> answer = solution(prices2);
+
+	cout << "answer: ";
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << " ";
+	}
+}
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer(prices.size());
+
+	int price_temp = 0;
+	for (int i = 0; i < prices.size(); i++) {
+		price_temp = prices[i];
+
+		for (int j = i; j < prices.size() - 1; j++) {
+			if (prices[j + 1] >= price_temp) {  //price의 다음 원소가 현재 temp 원소보다 크다면 1초 증가
+				answer[i] += 1;
+			}
+			else if (prices[j + 1] < price_temp) {
+				answer[i] += 1;
+				break;
+			}
+		}
+
+	}
+
+	return answer;
+}
