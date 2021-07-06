bb0a997876ad17ac0e57a96aeac32450d452820b
hhj3258 <hhj3258@gmail.com>
2021-07-06 17:27:04 +0900

    42577)전화번호_목록

--- Programmers/코딩테스트_고득점_Kit/해시/42577_전화번호_목록/42577_전화번호_목록.cpp
+++ Programmers/코딩테스트_고득점_Kit/해시/42577_전화번호_목록/42577_전화번호_목록.cpp
@@ -12,8 +12,14 @@ int main()
     vector<string> phone_book1 = {"119", "97674223", "1195524421"}; //false
 
     vector<string> phone_book2 = {"123", "456", "789"}; //true
+    bool answer = solution(phone_book2);
 
-    solution(phone_book1);
+    if (answer)
+        cout << "answer: "
+             << "no";
+    else
+        cout << "answer: "
+             << "yes";
 }
 
 bool solution(vector<string> phone_book)
@@ -21,14 +27,40 @@ bool solution(vector<string> phone_book)
     bool answer = true;
 
     set<string> book_set;
-
+    string xx = "xx";
+    xx.size();
     for (string phone_num : phone_book)
     {
         book_set.insert(phone_num);
     }
 
-    auto iter = book_set.find("119");
-    cout << (*iter)[0];
+    // auto iter = book_set.find("119");
+    // cout << (*iter)[0];
+
+    auto temp_num = book_set.begin();
+
+    for (auto i = book_set.begin(); i != book_set.end(); i++)
+    {
+        for (int j = 0; j < (*temp_num).size(); j++)
+        {
+            if (j == (*i).size())
+            {
+                break;
+            }
+
+            if ((*temp_num)[j] != (*i)[j])
+            {
+                answer = true;
+                break;
+            }
+            else
+            {
+                answer = false;
+            }
+        }
+
+        temp_num++;
+    }
 
     return answer;
 }
