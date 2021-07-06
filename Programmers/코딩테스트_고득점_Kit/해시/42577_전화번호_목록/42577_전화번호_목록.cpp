03119832a5c6c5b82336810cdf8a330e78a794ab
hhj3258 <hhj3258@gmail.com>
2021-07-06 14:37:32 +0900

    vscode setting 추가 및 파일명 변경

---
+++ Programmers/코딩테스트_고득점_Kit/해시/42577_전화번호_목록/42577_전화번호_목록.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+bool solution(vector<string> phone_book);
+
+int main()
+{
+    vector<string> phone_book1 = {"119", "97674223", "1195524421"}; //false
+
+    vector<string> phone_book2 = {"123", "456", "789"}; //true
+
+    solution(phone_book1);
+}
+
+bool solution(vector<string> phone_book)
+{
+    bool answer = true;
+
+    set<string> book_set;
+
+    for (string phone_num : phone_book)
+    {
+        book_set.insert(phone_num);
+    }
+
+    auto iter = book_set.find("119");
+    cout << (*iter)[0];
+
+    return answer;
+}
