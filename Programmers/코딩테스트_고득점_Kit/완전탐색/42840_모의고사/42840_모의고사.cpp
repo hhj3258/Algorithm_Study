112e15af863c61f89bba9f66923b04208791e9c4
hhj3258 <hhj3258@gmail.com>
2021-07-20 16:27:04 +0900

    max값 뽑아내는 로직 수정(max_element)

--- Programmers/코딩테스트_고득점_Kit/완전탐색/42840_모의고사/42840_모의고사.cpp
+++ Programmers/코딩테스트_고득점_Kit/완전탐색/42840_모의고사/42840_모의고사.cpp
@@ -2,56 +2,52 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 vector<int> solution(vector<int> answers)
 {
     vector<int> answer;
-    vector<vector<int>> mans(answers.size(), vector<int>(3));
-
-    vector<int> mans_cnt = {0, 0, 0};
+    vector<vector<int>> mans(3, vector<int>(answers.size()));
+    vector<int> mans_cnt(3);
 
     vector<int> man1 = {1, 2, 3, 4, 5};
     vector<int> man2 = {2, 1, 2, 3, 2, 4, 2, 5};
     vector<int> man3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-    for (int i = 0; i < mans.size(); i++)
+    for (int i = 0; i < answers.size(); i++)
     {
         //1번째 수포자
         if (mans_cnt[0] > man1.size() - 1)
             mans_cnt[0] = 0;
-        mans[i][0] = man1[mans_cnt[0]++];
+        mans[0][i] = man1[mans_cnt[0]++];
 
         //2번째 수포자
         if (mans_cnt[1] > man2.size() - 1)
             mans_cnt[1] = 0;
-        mans[i][1] = man2[mans_cnt[1]++];
+        mans[1][i] = man2[mans_cnt[1]++];
 
         //3번째 수포자
         if (mans_cnt[2] > man3.size() - 1)
             mans_cnt[2] = 0;
-        mans[i][2] = man3[mans_cnt[2]++];
+        mans[2][i] = man3[mans_cnt[2]++];
     }
 
-    vector<pair<int, int>> score;
-    for (int i = 1; i < 4; i++)
-        score.push_back(make_pair(0, i));
+    vector<int> score(3);
+    for (int i = 0; i < mans.size(); i++)
+    {
+        for (int j = 0; j < mans[i].size(); j++)
+        {
+            if (answers[j] == mans[i][j])
+                score[i]++;
+        }
+    }
 
-    for (int i = 0; i < answers.size(); i++)
-        for (int j = 0; j < 3; j++)
-            if (answers[i] == mans[i][j])
-                score[j].first++;
-
-    sort(score.begin(), score.end());
-
-    int max_v = score[2].first;
-
-    for (int i = 0; i < score.size(); i++)
-        if (max_v == score[i].first)
-            answer.push_back(score[i].second);
-
-    sort(answer.begin(), answer.end());
+    int max_v = *max_element(score.begin(), score.end());
+    for (int i = 0; i < 3; i++)
+    {
+        if (score[i] == max_v)
+            answer.push_back(i + 1);
+    }
 
     return answer;
 }
@@ -62,7 +58,7 @@ int main()
     vector<int> answers2 = {1, 3, 2, 4, 2};
     vector<int> answers3 = {1, 3, 2, 4, 2, 1, 3, 2, 4, 2, 1, 3, 2, 4, 2};
 
-    vector<int> answer = solution(answers3);
+    vector<int> answer = solution(answers2);
 
     cout << "answer: [";
     for (int i : answer)
