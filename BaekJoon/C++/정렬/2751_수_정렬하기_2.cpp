1b602895f0f9a49918944e4a3ab7d5aef175ab20
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-07-28 19:24:10 +0900

    2751_수_정렬하기_2

--- BaekJoon/C++/정렬/2751_수_정렬하기_2.cpp
+++ BaekJoon/C++/정렬/2751_수_정렬하기_2.cpp
@@ -1,66 +1,64 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<int> arr, arr2;
+int v1[1000000];
+int v2[1000000];
 
-//병합
 void merge(int left, int right)
 {
     int mid = (left + right) / 2;
+    int L = left;
+    int R = mid + 1;
+    int K = left;
 
-    int i = left;    //분할된 좌측 배열 첫번째 인덱스
-    int j = mid + 1; //분할된 우측 배열 첫번째 인덱스
-    int k = left;
-    //좌측을 모두 탐색하고 && 우측을 모두 탐색할 때까지
-    while (i <= mid && j <= right)
+    while (L <= mid && R <= right)
     {
-        if (arr[i] <= arr[j])
-        {
-            arr2[k++] = arr[i++];
-        }
+        if (v1[L] < v1[R])
+            v2[K++] = v1[L++];
         else
-            arr2[k++] = arr[j++];
+            v2[K++] = v1[R++];
     }
 
-    int temp = i > mid ? j : i;
+    int min_idx;
+    if (L <= mid)
+        min_idx = L;
+    else
+        min_idx = R;
 
-    while (k <= right)
+    while (K <= right)
     {
-        arr2[k++] = arr[temp++];
+        v2[K++] = v1[min_idx++];
     }
 
     for (int i = left; i <= right; i++)
-        arr[i] = arr2[i];
+        v1[i] = v2[i];
 }
 
-//분할
 void partition(int left, int right)
 {
-    int mid;
-    if (left < right)
-    {
-        mid = (left + right) / 2;
-        partition(left, mid);
-        partition(mid + 1, right);
-        merge(left, right);
-    }
+    if (left == right)
+        return;
+
+    int mid = (left + right) / 2;
+    partition(left, mid);
+    partition(mid + 1, right);
+    merge(left, right);
 }
 
 int main()
 {
     int n;
-    cin >> n;
+    scanf("%d", &n);
+
     int temp;
     for (int i = 0; i < n; i++)
     {
-        cin >> temp;
-        arr.push_back(temp);
-        arr2.push_back(0);
+        scanf("%d", &temp);
+        v1[i] = temp;
     }
 
-    partition(0, arr.size() - 1);
+    partition(0, n - 1);
 
     for (int i = 0; i < n; i++)
-        cout << arr[i] << endl;
+        printf("%d\n", v1[i]);
 }
