b1d69b8d47d5d29f383ff0f5eb90d3e476fb6d38
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-07-29 19:40:21 +0900

    2108_통계학

--- BaekJoon/C++/정렬/2108_통계학.cpp
+++ BaekJoon/C++/정렬/2108_통계학.cpp
@@ -2,62 +2,63 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <map>
 using namespace std;
+
+int MostNum(map<int, int> most_map)
+{
+    int max = -1;
+    int c = 0;
+    for (auto iter = most_map.begin(); iter != most_map.end(); iter++)
+    {
+        if ((*iter).second > max)
+        {
+            max = (*iter).second;
+            c = (*iter).first;
+        }
+    }
+
+    int cnt = 0;
+    for (auto iter = most_map.begin(); iter != most_map.end(); iter++)
+    {
+        if ((*iter).second == max)
+            cnt++;
+
+        if (cnt == 2)
+        {
+            c = (*iter).first;
+            break;
+        }
+    }
+
+    return c;
+}
 
 int main()
 {
     int N;
-
     cin >> N;
+
     vector<int> nums(N);
-    vector<int> positive_count(4001);
-    vector<int> negative_count(4001);
-    int temp;
-    float sum = 0;
+    map<int, int> most_map;
+
+    int temp = 0;
+    double sum = 0;
     for (int i = 0; i < N; i++)
     {
         cin >> temp;
         nums[i] = temp;
         sum += temp;
-
-        if (temp >= 0)
-            positive_count[temp]++;
-        else
-            negative_count[abs(temp)]++;
+        most_map[temp]++;
     }
 
     sort(nums.begin(), nums.end());
 
-    int a = round(sum / N);    //»ê¼úÆò±Õ
-    int b = nums[(N - 1) / 2]; //Áß¾Ó°ª
-    int c;
+    int a = round(sum / N);
+    int b = nums[N / 2];
+    int c = MostNum(most_map);
+    int d = nums[nums.size() - 1] - nums[0];
 
-    int positive_max = *max_element(positive_count.begin(), positive_count.end());
-    int negative_max = *max_element(negative_count.begin(), negative_count.end());
-
-    int real_max;
-    if (positive_max > negative_max)
-        real_max = positive_max;
-    else
-        real_max = negative_max;
-
-    int cnt = 0;
-    for (int i = negative_count.size() - 1; i >= 0; i--)
-    {
-
-        if (negative_count[i] == real_max)
-            cnt++;
-
-        if (cnt == 2)
-        {
-            c = (-1 * i);
-            break;
-        }
-    }
-
-    int d = nums[nums.size() - 1] - nums[0]; //¹üÀ§
-
-    cout << "answer" << endl;
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
