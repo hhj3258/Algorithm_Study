a819b83b1d1ff5610b358911274582c48d3068f5
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-07-30 18:53:50 +0900

    18870_좌표_압축

--- BaekJoon/C++/정렬/18870_좌표_압축.cpp
+++ BaekJoon/C++/정렬/18870_좌표_압축.cpp
@@ -3,22 +3,52 @@
 #include <algorithm>
 using namespace std;
 
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+    return a.second < b.second;
+}
+
+vector<pair<int, int>> CoordinateZip(vector<pair<int, int>> nums)
+{
+    sort(nums.begin(), nums.end());
+
+    int zip_num = -1;
+    int origin_temp = 1000000001;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i].first == origin_temp)
+        {
+            origin_temp = nums[i].first;
+            nums[i].first = zip_num;
+        }
+        else
+        {
+            origin_temp = nums[i].first;
+            nums[i].first = ++zip_num;
+        }
+    }
+
+    sort(nums.begin(), nums.end(), cmp);
+
+    return nums;
+}
+
 int main()
 {
     int N; //1<=N<=1,000,000
     cin >> N;
 
-    vector<int> nums;
+    vector<pair<int, int>> nums;
 
     int temp;
     for (int i = 0; i < N; i++)
     {
         cin >> temp;
-        nums.push_back(temp);
+        nums.push_back(make_pair(temp, i));
     }
 
-    vector<int> sorted_nums = nums;
-    sort(sorted_nums.begin(), sorted_nums.end());
+    nums = CoordinateZip(nums);
 
-    sorted_nums[0] = 0;
+    for (int i = 0; i < nums.size(); i++)
+        cout << nums[i].first << ' ';
 }
