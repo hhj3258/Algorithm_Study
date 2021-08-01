f368a67b703f04d3419d3eb5b7a435c169d07d67
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-08-01 17:41:33 +0900

    DFS

---
+++ Algorithm/DFS,BFS/DFS.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void dfs(int start, vector<int> graph[], bool check[])
+{
+    check[start] = true;
+    cout << start;
+
+    for (int i = 0; i < graph[start].size(); i++)
+    {
+        int next = graph[start][i];
+
+        if (check[next] == false)
+        {
+            dfs(next, graph, check);
+        }
+    }
+}
+
+int main()
+{
+    int n, m, start;
+    cin >> n >> m >> start;
+
+    vector<int> *graph = new vector<int>[n + 1];
+
+    bool *check = new bool[n + 1];
+    fill(check, check + n + 1, false);
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    dfs(start, graph, check);
+    printf("\n");
+}
