fb8d081118083c3c655f92feae5a9e2bb8368589
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-08-01 17:41:19 +0900

    15649_N과_M_1

--- BaekJoon/C++/백트래킹/15649_N과_M_1.cpp
+++ BaekJoon/C++/백트래킹/15649_N과_M_1.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define MAX 9
 
-int n, m;
-int arr[MAX] = {
-    0,
-};
-bool visited[MAX] = {
-    0,
-};
+int N, M;
 
-void dfs(int cnt)
+void dfs(int cnt, vector<int> vec, vector<bool> visited)
 {
-    if (cnt == m)
+    if (cnt == M)
     {
-        for (int i = 0; i < m; i++)
-            cout << arr[i] << ' ';
+        for (int i = 0; i < M; i++)
+            cout << vec[i] << ' ';
         cout << '\n';
         return;
     }
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < N; i++)
     {
         if (!visited[i])
         {
             visited[i] = true;
-            arr[cnt] = i;
-            dfs(cnt + 1);
+            vec[cnt] = i + 1;
+            dfs(cnt + 1, vec, visited);
             visited[i] = false;
         }
     }
@@ -35,4 +28,8 @@ void dfs(int cnt)
 
 int main()
 {
+    cin >> N >> M;
+    vector<int> vec(N);
+    vector<bool> visited(N, 0);
+    dfs(0, vec, visited);
 }
