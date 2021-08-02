f6b2594bc97ccd3505ee46811b275f875fdab804
hhj3258 <hhj3258@gmail.com>
2021-08-02 17:29:47 +0900

    15651_N과_M_3

--- BaekJoon/C++/백트래킹/15651_N과_M_3.cpp
+++ BaekJoon/C++/백트래킹/15651_N과_M_3.cpp
@@ -5,22 +5,20 @@ using namespace std;
 
 int N, M;
 
-void DFS(int cnt, vector<int> vec, vector<bool> visited)
+void DFS(int cnt, vector<int> nums)
 {
     if (cnt == M)
     {
         for (int i = 0; i < M; i++)
-        {
-            cout << vec[i] << ' ';
-        }
+            cout << nums[i] << ' ';
         cout << '\n';
         return;
     }
 
     for (int i = 0; i < N; i++)
     {
-        vec[cnt] = i + 1;
-        DFS(cnt + 1, vec, visited);
+        nums[cnt] = i + 1;
+        DFS(cnt + 1, nums);
     }
 }
 
@@ -29,5 +27,5 @@ int main()
     cin >> N >> M;
     vector<int> vec(N);
     vector<bool> visited(N);
-    DFS(0, vec, visited);
+    DFS(0, vec);
 }
