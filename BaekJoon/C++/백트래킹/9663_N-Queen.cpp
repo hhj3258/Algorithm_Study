6f5dd3226ba1d1ae80f422aab8b6648e776f1685
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-08-04 11:50:21 +0900

    N-Queen

--- BaekJoon/C++/백트래킹/9663_N-Queen.cpp
+++ BaekJoon/C++/백트래킹/9663_N-Queen.cpp
@@ -4,9 +4,8 @@
 using namespace std;
 
 int N; // 1 ≤ N < 15
-int answer = 0;
 
-void TVisite(vector<vector<bool>> &visited, int i, int j)
+vector<vector<bool>> TVisit(vector<vector<bool>> visited, int i, int j)
 {
     //가로축
     for (int k = 0; k < N; k++)
@@ -35,79 +34,37 @@ void TVisite(vector<vector<bool>> &visited, int i, int j)
         if ((i + n) == (N - 1) || (j - n) == 0)
             break;
     }
-}
 
-void FVisite(vector<vector<bool>> &visited, int i, int j)
-{
-    //가로축
-    for (int k = 0; k < N; k++)
-        visited[i][k] = false;
-    //세로축
-    for (int k = 0; k < N; k++)
-        visited[k][j] = false;
-    // 대각축(\)
-    int min_v = min(i, j);
-    int max_v = -1;
-    for (int n = min_v; max_v < N - 1; n--)
-    {
-        visited[i - n][j - n] = false;
-        max_v = max(i - n, j - n);
-    }
-    // 대각축(/)
-    for (int n = 0;; n++)
-    {
-        visited[i - n][j + n] = false;
-        if (i - n == 0 || j + n == N - 1)
-            break;
-    }
-    for (int n = 0;; n++)
-    {
-        visited[i + n][j - n] = false;
-        if ((i + n) == (N - 1) || (j - n) == 0)
-            break;
-    }
+    return visited;
 }
 
 int depth = 0;
+int answer = 0;
 
 void N_Queen(int qCnt, vector<vector<bool>> visited, int x, int y)
 {
-    cout << "qCnt: " << qCnt << endl;
-    if (qCnt == N - 1)
+    // cout << "xy[" << x << ", " << y << "]" << endl;
+    // cout << "qCnt: " << qCnt << endl;
+    if (qCnt == N)
     {
         answer++;
+        cout << answer << ' ';
+        //      << endl;
         return;
     }
 
-    for (int i = 0; i < N; i++)
+    for (int i = x; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             if (!visited[i][j])
             {
-                TVisite(visited, i, j);
-                /*
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        cout << visited[i][j] << ' ';
-                    }
-                    cout << '\n';
-                }
-
-                cout << "[" << i << ", " << j << "]" << endl;
-                cout << "now count: "
-                     << ++count << endl;
-                */
-                cout << "depth: " << ++depth << endl;
-                cout << i << ' ' << j << endl;
-                //cout << visited[i][j] << endl;
-
+                // cout << "chk: " << i << " " << j << endl
+                //      << endl;
+                vector<vector<bool>> temp = visited;
+                visited = TVisit(visited, i, j);
                 N_Queen(qCnt + 1, visited, i, j);
-
-                cout << "depth: " << --depth << endl;
-                FVisite(visited, i, j);
+                visited = temp;
             }
         }
     }
@@ -118,7 +75,11 @@ int main()
     cin >> N;
     vector<vector<bool>> visited(N, vector<bool>(N)); // N*N
 
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            visited[i][j] = false;
+
     N_Queen(0, visited, 0, 0);
 
-    cout << "answer: " << answer << endl;
+    cout << answer << endl;
 }
--- BaekJoon/C++/백트래킹/9663_N-Queen_copy.cpp
+++ BaekJoon/C++/백트래킹/9663_N-Queen_copy.cpp
@@ -4,93 +4,51 @@
 using namespace std;
 
 int N; // 1 ≤ N < 15
+int answer;
 
-vector<vector<bool>> TVisit(vector<vector<bool>> visited, int i, int j)
+void Visit(vector<vector<bool>> &visited, int i, int j)
 {
     //가로축
     for (int k = 0; k < N; k++)
+    {
         visited[i][k] = true;
-    //세로축
-    for (int k = 0; k < N; k++)
         visited[k][j] = true;
-    // 대각축(\)
-    int min_v = min(i, j);
-    int max_v = -1;
-    for (int n = min_v; max_v < N - 1; n--)
-    {
-        visited[i - n][j - n] = true;
-        max_v = max(i - n, j - n);
-    }
-    // 대각축(/)
-    for (int n = 0;; n++)
-    {
-        visited[i - n][j + n] = true;
-        if (i - n == 0 || j + n == N - 1)
-            break;
-    }
-    for (int n = 0;; n++)
-    {
-        visited[i + n][j - n] = true;
-        if ((i + n) == (N - 1) || (j - n) == 0)
-            break;
     }
 
-    return visited;
-}
-
-void FVisit(vector<vector<bool>> &visited, int i, int j)
-{
-    //가로축
-    for (int k = 0; k < N; k++)
-        visited[i][k] = false;
-    //세로축
-    for (int k = 0; k < N; k++)
-        visited[k][j] = false;
     // 대각축(\)
-    int min_v = min(i, j);
-    int max_v = -1;
-    for (int n = min_v; max_v < N - 1; n--)
-    {
-        visited[i - n][j - n] = false;
-        max_v = max(i - n, j - n);
-    }
-    // 대각축(/)
     for (int n = 0;; n++)
     {
-        visited[i - n][j + n] = false;
-        if (i - n == 0 || j + n == N - 1)
-            break;
-    }
-    for (int n = 0;; n++)
-    {
-        visited[i + n][j - n] = false;
-        if ((i + n) == (N - 1) || (j - n) == 0)
+        if (i + n < N)
+        {
+            if (j + n < N)
+                visited[i + n][j + n] = true;
+
+            if (j - n >= 0)
+                visited[i + n][j - n] = true;
+        }
+        else
             break;
     }
 }
 
-int depth = 0;
-int answer = 0;
-vector<vector<bool>> temp;
-void N_Queen(int qCnt, vector<vector<bool>> visited, int x, int y)
+void N_Queen(int qCnt, vector<vector<bool>> visited, int x)
 {
-    cout << "qCnt: " << qCnt << endl;
     if (qCnt == N)
     {
         answer++;
+        cout << answer << ' ';
         return;
     }
 
-    for (int i = 0; i < N; i++)
+    for (int i = x; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             if (!visited[i][j])
             {
-                cout << "[" << i << ", " << j << "]" << endl;
-                temp = visited;
-                visited = TVisit(visited, i, j);
-                N_Queen(qCnt + 1, visited, i, j);
+                vector<vector<bool>> temp = visited;
+                Visit(visited, i, j);
+                N_Queen(qCnt + 1, visited, i + 1);
                 visited = temp;
             }
         }
@@ -102,7 +60,7 @@ int main()
     cin >> N;
     vector<vector<bool>> visited(N, vector<bool>(N)); // N*N
 
-    N_Queen(0, visited, 0, 0);
+    N_Queen(0, visited, 0);
 
-    cout << "answer: " << answer << endl;
+    cout << answer << endl;
 }
