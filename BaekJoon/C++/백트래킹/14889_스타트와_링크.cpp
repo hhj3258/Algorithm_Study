c89fb1c3f1e4fbfb2090e9d93a1e1241167af26c
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-08-09 12:29:15 +0900

    14889_스타트와_링크

---
+++ BaekJoon/C++/백트래킹/14889_스타트와_링크.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int N; //4 ¡Â N ¡Â 20, NÀº Â¦¼ö
+int parameters[20][20];
+int diff = 1000000001;
+
+void TeamBuilding(int i, int j)
+{
+
+    for (int x = 0; x < N; x++)
+    {
+        for (int y = 0; y < N; y++)
+        {
+        }
+    }
+}
+
+int main()
+{
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> parameters[i][j];
+
+    TeamBuilding(0, 1);
+}
