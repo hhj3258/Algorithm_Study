3e19ca4563f13627a0d22406c05ad97e2cc08762
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-08-18 07:38:21 +0900

    week03_퍼즐_조각_채우기

--- Programmers/위클리_챌린지/week03_퍼즐_조각_채우기.cpp
+++ Programmers/위클리_챌린지/week03_퍼즐_조각_채우기.cpp
@@ -1,14 +1,15 @@
 #include <set>
-#include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-void Prints(vector<vector<int>> game_board);
-void Prints(vector<vector<bool>> game_board);
-vector<set<pair<int, int>>> PuzzleDiv(vector<vector<int>> &tables);
-vector<vector<pair<int, int>>> PuzzleReduction(vector<set<pair<int, int>>> test);
+vector<vector<pair<int, int>>> PuzzleDivision2(vector<vector<int>> &tables);
+vector<vector<pair<int, int>>> PuzzleRotation(vector<vector<pair<int, int>>> final_table);
+vector<vector<pair<int, int>>> PuzzleReduction(vector<vector<pair<int, int>>> test);
+int PuzzleMatching(vector<vector<pair<int, int>>> final_board, vector<vector<pair<int, int>>> final_table);
+void GameBoardReversing(vector<vector<int>> &my_board);
 
 vector<vector<int>> game_board = {{1, 1, 0, 0, 1, 0}, {0, 0, 1, 0, 1, 0}, {0, 1, 1, 0, 0, 1}, {1, 1, 0, 1, 1, 1}, {1, 0, 0, 0, 1, 0}, {0, 1, 1, 1, 0, 0}};
 vector<vector<int>> table = {{1, 0, 0, 1, 1, 0}, {1, 0, 1, 0, 1, 0}, {0, 1, 1, 0, 1, 1}, {0, 0, 1, 0, 0, 0}, {1, 1, 0, 1, 1, 0}, {0, 1, 0, 0, 0, 0}};
@@ -16,95 +17,96 @@ vector<vector<int>> table = {{1, 0, 0, 1, 1, 0}, {1, 0, 1, 0, 1, 0}, {0, 1, 1, 0
 vector<vector<int>> game_board2 = {{0, 0, 0}, {1, 1, 0}, {1, 1, 1}};
 vector<vector<int>> table2 = {{1, 1, 1}, {1, 0, 0}, {0, 0, 0}};
 
-vector<vector<int>> test_table = {{0, 0, 0, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 0, 0, 0}};
+vector<vector<int>> game_board3 = {{0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0}, {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1}, {0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1}, {0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0}, {0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0}, {1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1}, {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0}};
+vector<vector<int>> table3 = {{1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1}, {1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1}, {1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0}, {0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0}, {1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0}, {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1}, {1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1}, {0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1}, {1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1}, {1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1}, {1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1}};
 
 //-----------------------------------------------------------------------------------------------------
 
 int solution(vector<vector<int>> game_board, vector<vector<int>> table)
 {
-    int answer = -1;
+    GameBoardReversing(game_board);
+
+    vector<vector<pair<int, int>>> final_table = PuzzleDivision(table);
+
+    vector<vector<pair<int, int>>> final_board = PuzzleDivision(game_board);
+
+    int answer = PuzzleMatching(final_board, final_table);
+
     return answer;
 }
 
-int puzzle_idx = 0;
-//조각 나누기
-vector<set<pair<int, int>>> PuzzleDiv(vector<vector<int>> &tables)
+// vector<vector<bool>> visited(table.size(), vector<bool>(table.size()));
+vector<vector<pair<int, int>>> PuzzleDivision(vector<vector<int>> &tables)
 {
-    puzzle_idx = 0;
-    vector<set<pair<int, int>>> test;
-    for (int i = 0; i < tables.size(); i++)
+    vector<vector<pair<int, int>>> result;
+
+    while (true)
     {
-        for (int j = 0; j < tables[i].size(); j++)
+        vector<pair<int, int>> temp;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < tables.size(); i++)
         {
-            if (tables[i][j] == 1)
+            for (int j = 0; j < tables.size(); j++)
             {
-                set<pair<int, int>> temp;
-                temp.insert(make_pair(i, j));
-                test.push_back(temp);
-
-                for (auto iter = test[puzzle_idx].begin(); iter != test[puzzle_idx].end(); iter++)
+                if (tables[i][j] == 1)
                 {
-                    int nowI = (*iter).first;
-                    int nowJ = (*iter).second;
-
-                    if (nowJ < tables.size() - 1 && tables[nowI][nowJ + 1] == 1)
-                        test[puzzle_idx].insert(make_pair(nowI, nowJ + 1));
-
-                    if (nowJ > 0 && tables[nowI][nowJ - 1] == 1)
-                        test[puzzle_idx].insert(make_pair(nowI, nowJ - 1));
-
-                    if (nowI < tables.size() - 1 && tables[nowI + 1][nowJ] == 1)
-                        test[puzzle_idx].insert(make_pair(nowI + 1, nowJ));
-
-                    if (nowI > 0 && tables[nowI - 1][nowJ] == 1)
-                        test[puzzle_idx].insert(make_pair(nowI - 1, nowJ));
+                    q.push(make_pair(i, j));
+                    tables[i][j] = 0;
+                    break;
                 }
+            }
+            if (!q.empty())
+                break;
+        }
 
-                for (auto iter = test[puzzle_idx].begin(); iter != test[puzzle_idx].end(); iter++)
-                    tables[(*iter).first][(*iter).second] = 0;
+        if (q.empty())
+            break;
 
-                puzzle_idx++;
+        while (!q.empty())
+        {
+            int cur_i = q.front().first;
+            int cur_j = q.front().second;
+            temp.push_back(q.front());
+            //cout << q.front().first << "," << q.front().second << endl;
+            q.pop();
+
+            if (cur_j < tables.size() - 1 && tables[cur_i][cur_j + 1] == 1)
+            {
+                q.push(make_pair(cur_i, cur_j + 1));
+                tables[cur_i][cur_j + 1] = 0;
+            }
+
+            if (cur_j > 0 && tables[cur_i][cur_j - 1] == 1)
+            {
+                q.push(make_pair(cur_i, cur_j - 1));
+                tables[cur_i][cur_j - 1] = 0;
+            }
+
+            if (cur_i < tables.size() - 1 && tables[cur_i + 1][cur_j] == 1)
+            {
+                q.push(make_pair(cur_i + 1, cur_j));
+                tables[cur_i + 1][cur_j] = 0;
+            }
+
+            if (cur_i > 0 && tables[cur_i - 1][cur_j] == 1)
+            {
+                q.push(make_pair(cur_i - 1, cur_j));
+                tables[cur_i - 1][cur_j] = 0;
             }
         }
+
+        sort(temp.begin(), temp.end());
+        result.push_back(temp);
     }
 
-    return test;
-}
+    result = PuzzleReduction(result);
 
-//좌표 축소
-vector<vector<pair<int, int>>> PuzzleReduction(vector<set<pair<int, int>>> test)
-{
-    vector<vector<pair<int, int>>> tempV;
-
-    for (int i = 0; i < puzzle_idx; i++)
-    {
-        tempV.push_back(vector<pair<int, int>>(0));
-        for (auto iter = test[i].begin(); iter != test[i].end(); iter++)
-            tempV[i].push_back(*iter);
-
-        int minI = 100;
-        int minJ = 100;
-        for (int j = 0; j < tempV[i].size(); j++)
-        {
-            if (tempV[i][j].first < minI)
-                minI = tempV[i][j].first;
-            if (tempV[i][j].second < minJ)
-                minJ = tempV[i][j].second;
-        }
-        for (int j = 0; j < tempV[i].size(); j++)
-        {
-            tempV[i][j].first -= minI;
-            tempV[i][j].second -= minJ;
-        }
-    }
-
-    return tempV;
+    return result;
 }
 
 //반시계방향 회전 90도
-vector<vector<pair<int, int>>> TableRotation(vector<vector<pair<int, int>>> final_table)
+vector<vector<pair<int, int>>> PuzzleRotation(vector<vector<pair<int, int>>> final_table)
 {
-
     for (int i = 0; i < final_table.size(); i++)
     {
         int N = -1;
@@ -118,101 +120,109 @@ vector<vector<pair<int, int>>> TableRotation(vector<vector<pair<int, int>>> fina
 
         for (int j = 0; j < final_table[i].size(); j++)
             final_table[i][j] = make_pair(N - final_table[i][j].second, final_table[i][j].first);
+
+        sort(final_table[i].begin(), final_table[i].end());
     }
+
+    final_table = PuzzleReduction(final_table);
 
     return final_table;
 }
 
+//좌표 축소
+vector<vector<pair<int, int>>> PuzzleReduction(vector<vector<pair<int, int>>> test)
+{
+    for (int i = 0; i < test.size(); i++)
+    {
+        int minI = 100;
+        int minJ = 100;
+
+        for (int j = 0; j < test[i].size(); j++)
+        {
+            if (test[i][j].first < minI)
+                minI = test[i][j].first;
+            if (test[i][j].second < minJ)
+                minJ = test[i][j].second;
+        }
+
+        for (int j = 0; j < test[i].size(); j++)
+        {
+            test[i][j].first -= minI;
+            test[i][j].second -= minJ;
+        }
+    }
+
+    return test;
+}
+
+//퍼즐 맞추기
+int PuzzleMatching(vector<vector<pair<int, int>>> final_board, vector<vector<pair<int, int>>> final_table)
+{
+    int result = 0;
+
+    for (int r = 0; r < 4; r++)
+    {
+        for (int i = 0; i < final_board.size(); i++)
+        {
+            for (int j = 0; j < final_table.size(); j++)
+            {
+                if (final_board[i].size() == final_table[j].size())
+                {
+                    int cnt = 0;
+                    for (int k = 0; k < final_board[i].size(); k++)
+                    {
+                        if (final_board[i][k] == final_table[j][k])
+                            cnt++;
+                        else
+                        {
+                            cnt = 0;
+                            break;
+                        }
+                    }
+
+                    if (cnt == final_board[i].size())
+                    {
+                        final_board.erase(final_board.begin() + i);
+                        i--;
+                        final_table.erase(final_table.begin() + j);
+                        result += cnt;
+                        break;
+                    }
+                }
+            }
+        }
+
+        final_table = PuzzleRotation(final_table);
+    }
+
+    return result;
+}
+
+//게임보드 0-1 변환
+void GameBoardReversing(vector<vector<int>> &my_board)
+{
+    for (int i = 0; i < my_board.size(); i++)
+    {
+        for (int j = 0; j < my_board[i].size(); j++)
+        {
+            if (my_board[i][j] == 1)
+                my_board[i][j] = 0;
+            else
+            {
+                my_board[i][j] = 1;
+            }
+        }
+    }
+}
+
 int main()
 {
-    // Prints(game_board);
-    // Prints(table);
-    Prints(game_board);
+    GameBoardReversing(game_board);
 
-    vector<set<pair<int, int>>> puzzles = PuzzleDiv(table);
+    vector<vector<pair<int, int>>> final_table = PuzzleDivision(table);
 
-    cout << "main\n";
+    vector<vector<pair<int, int>>> final_board = PuzzleDivision(game_board);
 
-    // for (int i = 0; i < puzzle_idx; i++)
-    // {
-    //     for (auto iter = puzzles[i].begin(); iter != puzzles[i].end(); iter++)
-    //         cout << (*iter).first << ' ' << (*iter).second << endl;
-    //     cout << endl;
-    // }
-
-    vector<vector<pair<int, int>>> final_table = PuzzleReduction(puzzles);
-    final_table = TableRotation(final_table);
-
-    cout << "<puzzles>\n";
-    for (int i = 0; i < final_table.size(); i++)
-    {
-        for (int j = 0; j < final_table[i].size(); j++)
-            cout << final_table[i][j].first << ' ' << final_table[i][j].second << endl;
-        cout << endl;
-    }
-
-    //게임보드 0-1 변환
-    for (int i = 0; i < game_board.size(); i++)
-    {
-        for (int j = 0; j < game_board[i].size(); j++)
-        {
-            if (game_board[i][j] == 1)
-                game_board[i][j] = 0;
-            else
-                game_board[i][j] = 1;
-        }
-    }
-
-    vector<set<pair<int, int>>> boardBlanks = PuzzleDiv(game_board);
-    vector<vector<pair<int, int>>> final_board = PuzzleReduction(boardBlanks);
-
-    cout << "<board>\n";
-    for (int i = 0; i < final_board.size(); i++)
-    {
-        for (int j = 0; j < final_board[i].size(); j++)
-        {
-            cout << final_board[i][j].first << ' ' << final_board[i][j].second << endl;
-        }
-        cout << endl;
-    }
-
-    Prints(game_board);
-}
-
-void Prints(vector<vector<int>> game_board)
-{
-    int zero_cnt = 0;
-
-    cout << "<Print>\n";
-    for (int i = 0; i < game_board.size(); i++)
-    {
-        for (int j = 0; j < game_board[i].size(); j++)
-        {
-            if (game_board[i][j] == 0)
-                zero_cnt++;
-            cout << game_board[i][j] << ' ';
-        }
-        cout << endl;
-    }
-    cout << "zero_cnt: " << zero_cnt << endl;
-    cout << endl;
-}
-
-void Prints(vector<vector<bool>> game_board)
-{
-    int zero_cnt = 0;
-
-    cout << "<Visit>\n";
-    for (int i = 0; i < game_board.size(); i++)
-    {
-        for (int j = 0; j < game_board[i].size(); j++)
-        {
-            if (game_board[i][j] == 0)
-                zero_cnt++;
-            cout << game_board[i][j] << ' ';
-        }
-        cout << endl;
-    }
-    cout << "zero_cnt: " << zero_cnt << endl;
-    cout << endl;
+    int answer = PuzzleMatching(final_board, final_table);
+    cout << answer << endl;
 }
