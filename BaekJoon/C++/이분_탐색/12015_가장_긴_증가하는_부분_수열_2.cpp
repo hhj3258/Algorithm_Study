6cc6d8ac7fbf7ed1658e98af079d8293f8505b48
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-09-01 18:41:45 +0900

    주석추가) 12015_가장_긴_증가하는_부분_수열_2

--- BaekJoon/C++/이분_탐색/12015_가장_긴_증가하는_부분_수열_2.cpp
+++ BaekJoon/C++/이분_탐색/12015_가장_긴_증가하는_부분_수열_2.cpp
@@ -7,33 +7,26 @@ int N;
 
 void Solve(vector<int> A)
 {
-    int temp = 0;
-    int cnt = 0;
     vector<int> vec(1, 0);
 
     for (int i = 0; i < N; i++)
     {
-        temp = A[i];
-
-        //넣고자하는 값이 vec의 마지막 값보다 크다면 넣어주고
-        //길이 + 1
-        if (temp > vec.back())
-        {
-            vec.push_back(temp);
-            cnt++;
-        }
+        //넣고자하는 값이 vec의 마지막 값보다 크다면 넣어준다.
+        if (A[i] > vec.back())
+            vec.push_back(A[i]);
         //넣고자하는 값이 vec의 마지막 값보다 작거나 같다면
-        //temp를 기존 vec안에서 찾아본다.
-        //temp가 있다면 그 iter를 돌려줄 것이고, 없다면 temp보다 큰 원소중 가장 처음 오는 iter를 반환해준다.
-        //해당 iter에 해당되는 vec 값을 변경해준다.
         else
         {
-            auto low = lower_bound(vec.begin(), vec.end(), temp);
-            *low = temp;
+            //temp를 기존 vec안에서 찾아본다.
+            //temp가 있다면 그 iter를 돌려줄 것이고, 없다면 temp보다 큰 원소중 가장 처음 오는 iter를 반환해준다.
+            auto low = lower_bound(vec.begin(), vec.end(), A[i]);
+            //해당 iter에 해당되는 vec 값을 변경해준다.
+            *low = A[i];
         }
     }
 
-    cout << cnt << endl;
+    //초기값으로 넣어준 0을 제외한 vec의 길이가 가장 긴 증가하는 부분 수열의 길이가 된다.
+    cout << vec.size() - 1 << endl;
 }
 
 int main()
