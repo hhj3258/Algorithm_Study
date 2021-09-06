acb0338c5db29739b1f274681c4350f4036f9b85
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-09-07 00:48:03 +0900

    11050_이항_계수_1

---
+++ BaekJoon/C++/정수론_및_조합론/11050_이항_계수_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, K;
+
+int Binomial(int n, int r, vector<vector<int>> &dp)
+{
+    if (dp[n][r] > 0)
+        return dp[n][r];
+
+    if (r == 0 || n == r)
+        return dp[n][r] = 1;
+
+    return dp[n][r] = Binomial(n - 1, r - 1, dp) + Binomial(n - 1, r, dp);
+}
+
+int main()
+{
+    cin >> N >> K;
+    vector<vector<int>> dp(N + 1, vector<int>(K + 1));
+    cout << Binomial(N, K, dp);
+}
