1403ba667de309ef24e0c4042d00a0ffcb6c5b79
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-09-21 21:09:53 +0900

    리팩토링) 14503_로봇_청소기

--- BaekJoon/스터디/week01/14503_로봇_청소기.cpp
+++ BaekJoon/스터디/week01/14503_로봇_청소기.cpp
@@ -2,81 +2,57 @@
 using namespace std;
 
 int N, M;
-int r, c, d;
-//d = 0:북, 1:동, 2:남, 3:서
+int y, x, dir;
+
+bool CanGo(int y, int x, const vector<vector<int>> &room)
+{
+    if (room[y][x] == 0)
+        return true;
+    else
+        return false;
+}
 
 void Solve(vector<vector<int>> &room)
 {
-    room[r][c] = 2; // 초기 위치 청소
+    int dy[] = {0, -1, 0, 1};
+    int dx[] = {-1, 0, 1, 0};
+
+    int back_dy[] = {1, 0, -1, 0};
+    int back_dx[] = {0, -1, 0, 1};
 
     while (true)
     {
-        // 사방이 모두 빈 공간이 아니라면
-        if (room[r][c - 1] != 0 && room[r - 1][c] != 0 && room[r][c + 1] != 0 && room[r + 1][c] != 0)
-        {
-            //방향에 따른 후진
-            if (d == 0)
-                r += 1;
-            else if (d == 1)
-                c -= 1;
-            else if (d == 2)
-                r -= 1;
-            else
-                c += 1;
+        room[y][x] = 2;
 
-            //후진하고보니 벽이라면 종료
-            if (room[r][c] == 1)
+        bool isAllCleaned = true;
+        for (int i = 0; i < 4; i++)
+            if (room[y + dy[i]][x + dx[i]] == 0)
+                isAllCleaned = false;
+
+        if (isAllCleaned)
+        {
+            y = y + back_dy[dir];
+            x = x + back_dx[dir];
+
+            if (room[y][x] == 1)
                 break;
+            else
+                continue;
+        }
 
-            continue;
+        int nextY = y + dy[dir];
+        int nextX = x + dx[dir];
+
+        if (CanGo(nextY, nextX, room))
+        {
+            y = nextY;
+            x = nextX;
         }
 
-        //북쪽을 보고 있다면
-        if (d == 0)
-        {
-            //서쪽 칸이 빈 공간이라면
-            if (room[r][c - 1] == 0)
-            {
-                //서쪽 칸 청소
-                room[r][--c] = 2;
-                //청소기 방향을 서쪽으로 변경
-                d = 3;
-            }
-            //서쪽 칸이 벽 또는 청소한 곳이라면
-            else
-                //방향만 변경
-                d = 3;
-        }
-        else if (d == 1)
-        {
-            if (room[r - 1][c] == 0)
-            {
-                room[--r][c] = 2;
-                d = 0;
-            }
-            else
-                d = 0;
-        }
-        else if (d == 2)
-        {
-            if (room[r][c + 1] == 0)
-            {
-                room[r][++c] = 2;
-                d = 1;
-            }
-            else
-                d = 1;
-        }
-        else if (d == 3)
-        {
-            if (room[r + 1][c] == 0)
-            {
-                room[++r][c] = 2;
-                d = 2;
-            }
-            else
-                d = 2;
-        }
+        if (dir == 0)
+            dir += 3;
+        else
+            dir--;
     }
 
     int answer = 0;
@@ -91,7 +67,7 @@ void Solve(vector<vector<int>> &room)
 int main()
 {
     cin >> N >> M;
-    cin >> r >> c >> d;
+    cin >> y >> x >> dir;
 
     vector<vector<int>> room(N, vector<int>(M));
     for (int i = 0; i < N; i++)
