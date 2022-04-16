3bafc54f0e6692922179b6b72598ec2d4611619d
hhj3258 <hhj3258@gmail.com>
2022-04-16 14:40:15 +0900

    refactor : 2606_바이러스

--- BaekJoon/스터디/week02_DFS&BFS/2606_바이러스.cpp
+++ BaekJoon/스터디/week02_DFS&BFS/2606_바이러스.cpp
@@ -1,68 +1,53 @@
-﻿#include <bits/stdc++.h>
+﻿#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int com_cnt = 0;
+bool visited[101];
+int virus = 0;
 
-void Solve(vector<vector<int>> &map)
+void Solve(int start, const vector<vector<int>> &graph)
 {
-    vector<int> visited(com_cnt + 1, false);
     queue<int> q;
+    q.emplace(start);
+    visited[start] = true;
 
-    // 시작 정점은 무조건 1번 컴퓨터
-    q.push(1);
-    visited[1] = true;
-
-    int v = 0;
-    int cnt = 0;
     while (!q.empty())
     {
-        v = q.front();
+        int now_v = q.front();
         q.pop();
-        cnt++;
 
-        for (int i = 1; i <= com_cnt; i++)
+        for (auto &next_v : graph[now_v])
         {
-            if (!visited[i])
+            if (!visited[next_v])
             {
-                if (map[v][i] == 1)
-                {
-                    q.push(i);
-                    visited[i] = true;
-                }
+                q.emplace(next_v);
+                visited[next_v] = true;
+                virus++;
             }
         }
     }
-
-    // 1번 컴퓨터는 제외하고 출력
-    cout << cnt - 1;
 }
 
 int main()
 {
-    cin >> com_cnt;
+    int N;
+    cin >> N;
 
-    int lines = 0; //간선
-    cin >> lines;
+    int M;
+    cin >> M;
 
-    vector<vector<int>> map(com_cnt + 1, vector<int>(com_cnt + 1));
+    vector<vector<int>> graph(N + 1);
     int v1, v2;
-    for (int i = 0; i < lines; i++)
+    for (int i = 0; i < M; i++)
     {
         cin >> v1 >> v2;
 
-        map[v1][v2] = 1;
-        map[v2][v1] = 1;
+        graph[v1].emplace_back(v2);
+        graph[v2].emplace_back(v1);
     }
 
-    // cout << "0 1 2 3 4 5 6 7" << '\n';
-    // for (int i = 0; i < map.size(); i++)
-    // {
-    //     for (int j = 0; j < map.size(); j++)
-    //     {
-    //         cout << map[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
+    Solve(1, graph);
 
-    Solve(map);
+    cout << virus;
 }
